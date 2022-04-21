6816bbd7698255aff09ad657598ebb38cfe7f68a
Geocarlos <galves@inair.studio>
2022-04-21 20:30:41 -0300

    Change template placeholder message

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -3,6 +3,11 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
-const App = () => <div className="container">Hello from InAir Studio!</div>;
+const App = () => (
+    <div className="container">
+        <h2>Hello from InAir Studio!</h2>
+        <p>Let's build an app using React-Bootstrap!</p>
+    </div>
+);
 
 initializeBlock(() => <App/>);
